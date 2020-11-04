8445cefd5949c3871d53adaeb069ab84de456d46
vagrant <avedj94@hotmail.com>
2020-11-04 21:38:30 +0000

    return false/read in reverse

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+ 
+    for (let line of horizontalJoin) {
+        let rev = line.split("").reverse().join("");
+        if (rev.includes(word)) return true;
+        // console.log(rev);
+    }
+    return false;
 }
+
 
 module.exports = wordSearch
